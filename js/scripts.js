e9118c2c60b88497d48e664af60594fea6e0f449
Marina-Dr <dronovamarina+github@gmail.com>
2022-10-22 22:54:17 +0200

    Update scripts.js
    added a loop that iterates over each item in pokemonList.  Within the loop, added a conditional.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-let pokemonList = [
+let pokemonList = [];
+pokemonList.push(
   {
     name:'Bulbasaur',
     height: 7,
@@ -19,4 +20,13 @@ let pokemonList = [
     height: 6,
     types: ['fire']
   }
-];
+);
+
+for (let i=0; i<pokemonList.length; i++){
+  document.write (pokemonList[i].name  + ' (height: '+ pokemonList[i].height + ')' );
+  if (pokemonList[i].height > 6) {
+    document.write ('Wow, that’s big! <br>');
+  } else  {
+    document.write ('<br>');
+  }
+}
